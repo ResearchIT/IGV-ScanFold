911f7d40a2c0cc77ad6f6cd1c8d509a2e0e9c2e8
Nick Booher <njbooher@iastate.edu>
2021-02-10 13:26:24 -0600

    Support spaces in folder names

--- scripts/run_igv.sh
+++ scripts/run_igv.sh
@@ -1,3 +1,3 @@
 export SCANFOLDRUNDIR=${PWD}
-export SCANFOLDRUNSCRIPT=${SCANFOLDRUNDIR}/scripts/run_scanfold.sh
+export SCANFOLDRUNSCRIPT=${SCANFOLDRUNDIR}/scripts/run_scanfold.py
 ./igv/build/IGV-dist/igv.sh
---
+++ scripts/run_scanfold.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python3
+
+import argparse
+import subprocess
+import tempfile
+import sys
+import os
+
+def run_me(workdir, command):
+    proc_env = os.environ.copy()
+    proc_env['DATAPATH'] = os.path.join(os.getcwd(), 'env', 'data_tables')
+    proc_env['VIRTUAL_ENV'] = os.path.join(os.getcwd(), 'env')
+    proc_env['PATH'] = ':'.join([os.path.join(os.getcwd(), 'env', 'bin'), proc_env['PATH']])
+    python_interpreter = ['python', '-u']
+    process = subprocess.Popen(python_interpreter + command, stdout=subprocess.PIPE, env=proc_env, cwd=workdir, bufsize=0)
+    for c in iter(lambda: process.stdout.read(1), b''): 
+        sys.stdout.buffer.write(c)
+        sys.stdout.buffer.flush()
+
+def mktemp(directory, extension, name="output"):
+    file_handle, file_path = tempfile.mkstemp(prefix=name, suffix=extension, dir=directory)
+    os.close(file_handle)
+    return file_path
+
+def main(args):
+
+    #INPUTFNAME = os.path.basename(args.INPUTFILE)
+    SCRIPTDIR = os.path.join(os.getcwd(), 'ScanFold')
+
+    SCANOUTPATH = mktemp(args.WORKDIR, '.scan-out.tsv')
+    ZSCOREWIGFILEPATH = mktemp(args.WORKDIR, '.zscore.wig')
+    MFEWIGFILEPATH = mktemp(args.WORKDIR, '.mfe.wig')
+    EDWIGFILEPATH = mktemp(args.WORKDIR, '.ed.wig')
+    PVALUEWIGFILEPATH = mktemp(args.WORKDIR, '.pvalue.wig')
+    FASTAFILEPATH = mktemp(args.WORKDIR, '.fasta', name='input')
+    FASTAINDEX = mktemp(args.WORKDIR, '.fasta.fai', name='input')
+
+    scan_params = [
+        os.path.join(SCRIPTDIR, 'ScanFold-Scan_IGV.py'),
+        '-i', args.INPUTFILE,
+        '-r', args.RANDOMIZATIONS,
+        '-s', args.STEPSIZE,
+        '-t', args.TEMPERATURE,
+        '-w', args.WINDOWSIZE,
+        '-d', args.STRAND,
+        '--start', args.STARTPOS,
+        '--name', args.SEQUENCENAME,
+        '-type', args.RANDOMIZATIONTYPE,
+        '--scan_out_path', SCANOUTPATH,
+        '--zscore_wig_file_path', ZSCOREWIGFILEPATH,
+        '--mfe_wig_file_path', MFEWIGFILEPATH,
+        '--ed_wig_file_path', EDWIGFILEPATH,
+        '--pvalue_wig_file_path', PVALUEWIGFILEPATH,
+        '--fasta_file_path', FASTAFILEPATH,
+        '--fasta_index', FASTAINDEX
+    ]
+
+    run_me(args.WORKDIR, scan_params)
+
+    OUT1 = mktemp(args.WORKDIR, '.nofilter.ct')
+    OUT2 = mktemp(args.WORKDIR, '.-1filter.ct')
+    OUT3 = mktemp(args.WORKDIR, '.-2filter.ct')
+    LOGFILE = mktemp(args.WORKDIR, '.log.txt')
+    FINALPARTNERS = mktemp(args.WORKDIR, '.final_partners.txt')
+    BPTRACK = mktemp(args.WORKDIR, '.IGV.bp')
+    FASTATRACK = mktemp(args.WORKDIR, '.input.fa')
+    #DBNFILEPATH = mktemp(args.WORKDIR, '.-2filter.dbn')
+    DBNFILEPATH = mktemp(args.WORKDIR, '.-2filter.scanfoldvarna')
+    DBNFILEPATH1 = mktemp(args.WORKDIR, '.dbnfile1.dbn')
+    DBNFILEPATH2 = mktemp(args.WORKDIR, '.dbnfile2.dbn')
+    DBNFILEPATH3 = mktemp(args.WORKDIR, '.dbnfile3.dbn')
+    DBNFILEPATH4 = mktemp(args.WORKDIR, '.DBNstructures.txt')
+    STRUCTUREEXTRACTFILE = mktemp(args.WORKDIR, '.ExtractedStructures.txt')
+    FINALPARTNERSWIG = mktemp(args.WORKDIR, '.final_partners_zscore.wig')
+    FASTAINDEX = mktemp(args.WORKDIR, '.fai')
+
+    fold_params = [
+        os.path.join(SCRIPTDIR, 'ScanFold-Fold_IGV.py'),
+        '-i', SCANOUTPATH,
+        '-d', args.STRAND,
+        '--name', args.SEQUENCENAME,
+        '-c', args.COMPETITION,
+        '--out1', OUT1,
+        '--out2', OUT2,
+        '--out3', OUT3,
+        '--out4', LOGFILE,
+        '--out5', FINALPARTNERS,
+        '--out6', BPTRACK,
+        '--out7', FASTATRACK,
+        '--dbn_file_path', DBNFILEPATH,
+        '--dbn_file_path1', DBNFILEPATH1,
+        '--dbn_file_path2', DBNFILEPATH2,
+        '--dbn_file_path3', DBNFILEPATH3,
+        '--dbn_file_path4', DBNFILEPATH4,
+        '--final_partners_wig', FINALPARTNERSWIG,
+        '--structure_extract_file', STRUCTUREEXTRACTFILE,
+        '--fasta_index', FASTAINDEX
+    ]
+
+    if args.GLOBALREFOLD:
+        fold_params.append('--global_refold')
+
+    run_me(args.WORKDIR, fold_params)
+
+    files_to_load = [
+        BPTRACK,
+        FINALPARTNERSWIG,
+        MFEWIGFILEPATH,
+        ZSCOREWIGFILEPATH,
+        PVALUEWIGFILEPATH,
+        EDWIGFILEPATH,
+        DBNFILEPATH,
+    ]
+
+    batch_file_path = os.path.join(args.WORKDIR, 'batchfile.txt')
+
+    with open(batch_file_path, 'w') as batch_file:
+        template = "load \"{}\"\n"
+        for file_to_load in files_to_load:
+            batch_file.write(template.format(file_to_load))
+
+    print("BATCHFILEFIRSTSENTINEL{}BATCHFILESECONDSENTINEL".format(batch_file_path))
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('-c', '--COMPETITION', type=str)
+    parser.add_argument('-g', '--GLOBALREFOLD', action='store_true')
+    parser.add_argument('-i', '--INPUTFILE', type=str)
+    parser.add_argument('-n', '--SEQUENCENAME', type=str)
+    parser.add_argument('-r', '--RANDOMIZATIONS', type=str)
+    parser.add_argument('-s', '--STEPSIZE', type=str)
+    parser.add_argument('-t', '--TEMPERATURE', type=str)
+    parser.add_argument('-w', '--WINDOWSIZE', type=str)
+    parser.add_argument('-y', '--RANDOMIZATIONTYPE', type=str)
+    parser.add_argument('-z', '--STARTPOS', type=str)
+    parser.add_argument('-d', '--STRAND', type=str)
+    parser.add_argument('-o', '--WORKDIR', type=str)
+
+    args = parser.parse_args()
+    
+    main(args)
