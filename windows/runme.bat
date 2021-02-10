5ecbf7344fe90fca251dfdfa4f5ba2ab07e2d9a3
Nick Booher <njbooher@iastate.edu>
2021-02-10 16:21:47 -0600

    Missed a spot

--- windows/runme.bat
+++ windows/runme.bat
@@ -6,7 +6,7 @@ for %%x in (%BatchPath%) do set BatchPath=%%~dpsx
 
 SET SCANFOLDRUNDIR=%BatchPath%
 SET SCANFOLDPYTHONINTERPRETER=%SCANFOLDRUNDIR%\scanfold\python-3.8.6-embed-amd64\python
-SET SCANFOLDRUNSCRIPT=%SCANFOLDRUNDIR%\scanfold\runscanfold.bat
+SET SCANFOLDRUNSCRIPT=%SCANFOLDRUNDIR%\scanfold\run_scanfold.py
 
 cd %BatchPath%\IGV
 
