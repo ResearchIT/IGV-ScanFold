6401c860891dfa8e3b924c50e705eff020a93fd1
Nick Booher <njbooher@iastate.edu>
2021-02-15 13:27:54 -0600

    Compile scanfold with nuitka

--- macos/bundle_macos.sh
+++ macos/bundle_macos.sh
@@ -17,6 +17,26 @@ unzip build/distributions/IGV_MacApp_user_WithJava.zip
 mv IGV_User.app ${BUNDLE_PREFIX}/IGV.app
 popd
 
+
+# compile scanfold
+pushd ScanFold
+sed -i 's/#!\/usr\/local\/bin\/python.*//' ScanFold-Scan_IGV.py ScanFold-Fold_IGV.py
+python3 -mvenv env
+. env/bin/activate
+pip install Nuitka biopython numpy
+python -m nuitka --standalone --plugin-enable=numpy ScanFold-Scan_IGV.py
+python -m nuitka --standalone --plugin-enable=numpy ScanFold-Fold_IGV.py
+deactivate
+popd
+
+# bundle scanfold
+mkdir -p ${BUNDLE_PREFIX}/scanfold/
+
+pushd ${BUNDLE_PREFIX}/scanfold
+cp -R ${TOPLEVEL}/ScanFold/{ScanFold-Scan_IGV.dist,ScanFold-Fold_IGV.dist} .
+cp ${TOPLEVEL}/scripts/run_scanfold.py .
+popd
+
 # main
 pushd ${BUNDLE_PREFIX}
 zip -r ${TOPLEVEL}/scanfoldigv-macos.zip *
