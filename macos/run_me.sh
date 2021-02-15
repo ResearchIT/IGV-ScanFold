3ee2c6b303784a061af98827ac20a5ac4cf1f107
Nick Booher <njbooher@iastate.edu>
2021-02-15 17:34:25 -0600

    Change multiprocessing method to work with nuitka

--- macos/run_me.sh
+++ macos/run_me.sh
@@ -2,5 +2,6 @@ export SCANFOLDRUNDIR=${PWD}
 export SCANFOLDPYTHONINTERPRETER=$(which python)
 export SCANFOLDRUNSCRIPT=${SCANFOLDRUNDIR}/scanfold/run_scanfold.py
 export SCANFOLDISBUNDLED=TRUE
+export SCANFOLDMPMETHOD=fork
 xattr -r -d com.apple.quarantine .
 open IGV.app
