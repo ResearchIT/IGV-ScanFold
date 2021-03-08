07f79daf6bb9244cd2e8978d5ee25bb9fbf891ec
Nick Booher <njbooher@iastate.edu>
2021-03-08 16:17:42 -0600

    handle non-c drives

--- windows/runme.bat
+++ windows/runme.bat
@@ -1,13 +1,9 @@
 setlocal
 
-::Get the current batch file's short path
-for %%x in (%0) do set BatchPath=%%~dpsx
-for %%x in (%BatchPath%) do set BatchPath=%%~dpsx
-
-SET SCANFOLDRUNDIR=%BatchPath%
+SET SCANFOLDRUNDIR=%cd%
 SET SCANFOLDPYTHONINTERPRETER=%SCANFOLDRUNDIR%\scanfold\python-3.8.6-embed-amd64\python
 SET SCANFOLDRUNSCRIPT=%SCANFOLDRUNDIR%\scanfold\run_scanfold.py
 
-cd %BatchPath%\IGV
+cd %cd%\IGV
 
-igv
+igv-launcher
